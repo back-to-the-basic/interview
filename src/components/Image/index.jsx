ef35a8efec95f393370b0f19ed457bc59b29b150
hyunjin <leehj0110@kakao.com>
2023-01-06 13:32:14 +0900

    chore: image 컴포넌트에 출처 기능 추가

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,20 +1,35 @@
-import React from "react"
+import React from 'react';
+import Link from '@docusaurus/Link';
 
-const Image = ({ src, size }) => {
+const Image = ({ src, size, referenceLink }) => {
   return (
-    <>
+    <div style={{ padding: 'auto', margin: 'auto' }}>
       <img
         src={src}
         width={size}
         height={size}
         style={{
-          display: "block",
-          margin: "auto",
+          display: 'block',
+          margin: 'auto',
         }}
       />
       <br />
-    </>
-  )
-}
 
-export default Image
+      {referenceLink && (
+        <Link
+          to={referenceLink}
+          style={{
+            width: '100%',
+            display: 'block',
+            margin: 'auto',
+            textAlign: 'center',
+          }}
+        >
+          그림 출처
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default Image;
