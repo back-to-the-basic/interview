952a5adb4eda8c00a87c30cafe8ca35484926572
hyunjin <leehj0110@kakao.com>
2023-01-11 16:29:16 +0900

    chore: add pwa features

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -123,6 +123,36 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      plugins: [
+        [
+          '@docusaurus/plugin-pwa',
+          {
+            debug: true,
+            offlineModeActivationStrategies: [
+              'appInstalled',
+              'standalone',
+              'queryString',
+            ],
+            pwaHead: [
+              {
+                tagName: 'link',
+                rel: 'icon',
+                href: '/img/docusaurus.png',
+              },
+              {
+                tagName: 'link',
+                rel: 'manifest',
+                href: '/manifest.json', // your PWA manifest
+              },
+              {
+                tagName: 'meta',
+                name: 'theme-color',
+                content: 'rgb(37, 194, 160)',
+              },
+            ],
+          },
+        ],
+      ],
     }),
 };
 
