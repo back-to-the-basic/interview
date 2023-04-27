de9eeae718c0033c22b342bbea363dfb53fa9d75
hyunjin <leehj0110@kakao.com>
2023-04-27 09:47:56 +0900

    chore: add icons

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,12 +6,10 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula")
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
+  title: "Interview",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
-
-  // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://bttb-interview.vercel.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -64,7 +62,7 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
+        title: "Interview",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
@@ -89,12 +87,12 @@ const config = {
         links: [
           {
             title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
+            // items: [
+            //   {
+            //     label: "Tutorial",
+            //     to: "/docs/intro",
+            //   },
+            // ],
           },
           {
             title: "Community",
@@ -168,3 +166,169 @@ const config = {
 }
 
 module.exports = config
+
+// /** @type {import('@docusaurus/types').Config} */
+// const config = {
+//   title: 'Interview',
+//   tagline: '우왕',
+//   url: 'https://bttb-interview.vercel.app',
+//   baseUrl: '/',
+//   onBrokenLinks: 'throw',
+//   onBrokenMarkdownLinks: 'warn',
+//   favicon: 'img/favicon.ico',
+//   i18n: {
+//     defaultLocale: 'ko',
+//     locales: ['ko'],
+//   },
+//   themeConfig: {},
+//   presets: [
+//     [
+//       'classic',
+//       /** @type {import('@docusaurus/preset-classic').Options} */
+//       ({
+//         docs: {
+//           sidebarPath: require.resolve('./sidebars.js'),
+//           editUrl: 'https://github.com/back-to-the-basic/interview',
+//         },
+//         theme: {
+//           customCss: require.resolve('./src/css/custom.css'),
+//         },
+//       }),
+//     ],
+//   ],
+//   themeConfig:
+//     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+//     ({
+//       navbar: {
+//         title: 'Interview',
+//         logo: {
+//           alt: 'My Site Logo',
+//           src: 'img/logo.svg',
+//         },
+//         items: [
+//           {
+//             type: 'doc',
+//             docId: 'operating-system/면접/프로세스와 스레드/프로세스와 스레드',
+//             position: 'left',
+//             label: '운영체제',
+//           },
+//           {
+//             type: 'doc',
+//             docId: 'network/google.com',
+//             position: 'left',
+//             label: 'Network',
+//           },
+//           {
+//             type: 'doc',
+//             docId: 'computer-structure/개념/1',
+//             position: 'left',
+//             label: '컴퓨터구조',
+//           },
+//           {
+//             type: 'doc',
+//             docId: 'react/Flux',
+//             position: 'left',
+//             label: 'React',
+//           },
+//           {
+//             type: 'doc',
+//             docId: 'javascript/1',
+//             position: 'left',
+//             label: 'JavaScript',
+//           },
+//           {
+//             type: 'doc',
+//             docId: 'web/쿠키와세션',
+//             position: 'left',
+//             label: 'WEB',
+//           },
+//           {
+//             href: 'https://github.com/back-to-the-basic/interview',
+//             label: 'GitHub',
+//             position: 'right',
+//           },
+//         ],
+//       },
+//       plugins: [
+//         [
+//           '@docusaurus/plugin-pwa',
+//           {
+//             debug: true,
+//             offlineModeActivationStrategies: [
+//               'appInstalled',
+//               'standalone',
+//               'queryString',
+//             ],
+//             pwaHead: [
+//               {
+//                 tagName: 'link',
+//                 rel: 'icon',
+//                 href: '/img/docusaurus.png',
+//               },
+//               {
+//                 tagName: 'link',
+//                 rel: 'manifest',
+//                 href: '/manifest.json', // your PWA manifest
+//               },
+//               {
+//                 tagName: 'meta',
+//                 name: 'theme-color',
+//                 content: 'rgb(37, 194, 160)',
+//               },
+//             ],
+//           },
+//         ],
+//       ],
+//       // footer: {
+//       //   style: "dark",
+//       //   links: [
+//       //     {
+//       //       title: "Docs",
+//       //       items: [
+//       //         {
+//       //           label: "Tutorial",
+//       //           to: "/docs/intro",
+//       //         },
+//       //       ],
+//       //     },
+//       //     {
+//       //       title: "Community",
+//       //       items: [
+//       //         {
+//       //           label: "Stack Overflow",
+//       //           href: "https://stackoverflow.com/questions/tagged/docusaurus",
+//       //         },
+//       //         {
+//       //           label: "Discord",
+//       //           href: "https://discordapp.com/invite/docusaurus",
+//       //         },
+//       //         {
+//       //           label: "Twitter",
+//       //           href: "https://twitter.com/docusaurus",
+//       //         },
+//       //       ],
+//       //     },
+//       //     {
+//       //       title: "More",
+//       //       items: [
+//       //         {
+//       //           label: "Blog",
+//       //           to: "/blog",
+//       //         },
+//       //         {
+//       //           label: "GitHub",
+//       //           href: "https://github.com/facebook/docusaurus",
+//       //         },
+//       //       ],
+//       //     },
+//       //   ],
+//       //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+//       // },
+//       prism: {
+//         theme: lightCodeTheme,
+//         darkTheme: darkCodeTheme,
+//       },
+//     }),
+// };
+
+// module.exports = config;
